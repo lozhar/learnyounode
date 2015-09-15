26db6724f53894f16557d40a7ac9a5851eb67f3b
Lorenzo Ferrante <lozhar@mail.com>
2015-09-15 17:52:25 -0500

    My First Async I/O solution

--- program.js
+++ program.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 var path = process.argv[2];
+var encoding = "utf8";
 
-var buf = fs.readFileSync(path);
-var str = buf.toString().split("\n").length - 1;
-console.log(str);
+var str = fs.readFile(path, encoding, function(err, data) {
+	if (err) {
+		return err;
+	}
+	console.log(data.split("\n").length - 1);
+});

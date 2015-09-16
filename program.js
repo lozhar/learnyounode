c2c96883afb6c27def4722e23248a4e9f71ec36b
Lorenzo Ferrante <lozhar@mail.com>
2015-09-15 19:19:46 -0500

    Http collect solution

--- program.js
+++ program.js
@@ -1,12 +1,16 @@
 var http = require("http");
 var url = process.argv[2];
 
-http.get(url, function(response) {
+http.get(url, function (response) {
+	var completeData = new Array();
 	response.setEncoding("utf8");
-	response.on("data", function (data) {
-		console.log(data);
+	response.on("data", function (chunk) {
+		completeData += chunk;
 	});
-
-	response.on("error", console.error);
+	response.on("error" , console.error);
+	response.on("end", function () {
+		console.log(completeData.length);
+		console.log(completeData);
+	});
 
 });

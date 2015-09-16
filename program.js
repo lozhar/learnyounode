224d8268942bb5336690f1f9601be438bd425519
Lorenzo Ferrante <lozhar@mail.com>
2015-09-15 19:03:20 -0500

    Http client solution

--- program.js
+++ program.js
@@ -1,12 +1,12 @@
-var filter = require("./module");
-var path = process.argv[2];
-var extension = process.argv[3];
-filter(path, extension, function(err, data) {
-	if (err) {
-		return err;
-	}
-	for (var i = 0; i<= data.length -1; i ++){
-		console.log(data[i]);
-	}
-	
+var http = require("http");
+var url = process.argv[2];
+
+http.get(url, function(response) {
+	response.setEncoding("utf8");
+	response.on("data", function (data) {
+		console.log(data);
+	});
+
+	response.on("error", console.error);
+
 });

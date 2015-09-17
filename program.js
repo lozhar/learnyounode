6ab3c2a25d2cb03cdbaee0628c5312e8f7e11512
Lorenzo Ferrante <lozhar@mail.com>
2015-09-16 19:06:05 -0500

    TIme server solution

--- program.js
+++ program.js
@@ -1,16 +1,32 @@
-var http = require("http");
-var url = process.argv[2];
+var net = require('net');
+var port = process.argv[2];
 
-http.get(url, function (response) {
-	var completeData = new Array();
-	response.setEncoding("utf8");
-	response.on("data", function (chunk) {
-		completeData += chunk;
-	});
-	response.on("error" , console.error);
-	response.on("end", function () {
-		console.log(completeData.length);
-		console.log(completeData);
-	});
+
+var server = net.createServer(function (socket) {
+	function addZero (num) {
+		if (num < 10) {
+			var theHour = "0" + num;
+			return theHour;
+		}
+		else {
+			return num;
+		}
+	}
+
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = addZero(date.getMonth() + 1);
+	var day = date.getDate();
+	var hours = addZero(date.getHours());
+	var minutes = addZero(date.getMinutes());
+    var data = year+"-"+month+"-"+day+" "+hours+":"+minutes;
+
+
+    socket.end(data);
+
 
 });
+
+server.listen(port);
+
+

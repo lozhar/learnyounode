419445cdd58fbfd162da4d324576f52855620e41
Lorenzo Ferrante <lozhar@mail.com>
2015-09-16 19:45:20 -0500

    Http uppercaserer solution

--- program.js
+++ program.js
@@ -1,10 +1,15 @@
-var http = require('http')
-var fs = require('fs')
+var http = require("http");
+var map = require("through2-map");
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'content-type': 'text/plain' })
+	if(req.method != "POST") {
+		return res.end("Not a post req")
+	}
 
-  fs.createReadStream(process.argv[3]).pipe(res)
-})
+	req.pipe(map(function (chunk) {
+		return chunk.toString().toUpperCase()
+	})).pipe(res)
 
-server.listen(Number(process.argv[2]))
+});
+
+server.listen(Number(process.argv[2]));
